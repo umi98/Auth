5b1accb3dfe7a63fa4d5264b71c421cf122a2489
umi98 <umifarida98@gmail.com>
2023-03-30 13:40:28 +0700

    Revisi

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -40,116 +40,110 @@ class Program
                     Console.Write("Password: ");
                     password = Console.ReadLine();
 
-                    username = firstname.Substring(0, 2) + lastname.Substring(0, 2);
-
-                    var cek = userList.Where(u => u.Username == username);
-                    
-                    if (cek.Count() >= 1)
+                    while (!cekPassword(password))
                     {
-                        Console.WriteLine("Firstname dan Lastname sudah tersedia");
-                    }
-                    else
-                    {
-                        if (password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsNumber))
-                        {
-                            User user = new User();
-
-                            user.Username = username;
-                            user.Firstname = firstname;
-                            user.Lastname = lastname;
-                            user.Password = password;
-
-                            userList.Add(user);
-
-                            Console.WriteLine("User berhasil ditambah. Tekan sembarang tombol untuk kembali ke halaman utama");
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            Console.WriteLine("User gagal ditambah, pastikan panjang password minimal 8, terdapat minimal 1 huruf kapital dan 1 angka");
-                            Console.ReadKey();
-                        }
-                    }
-                    
-                    break;
-                case "2":
-                    Console.Clear();
-                    Console.WriteLine("List of User(s)");
-
-                    int index = 1;
-                    foreach(User u in userList)
-                    {
-                        Console.WriteLine("--------------------------------");
-                        Console.WriteLine("ID: " + index);
-                        Console.WriteLine("Username: " + u.Username);
-                        Console.WriteLine("Name: " + u.Firstname + " " + u.Lastname);
-                        Console.WriteLine("Password: " + u.Password);
-                        index++;
-                    }
-
-                    Console.WriteLine("Menu.");
-                    Console.WriteLine("1. Edit user");
-                    Console.WriteLine("2. Delete user");
-                    Console.WriteLine("3. Return to main menu");
-                    Console.Write("Your option? ");
-                    inputMenu = Console.ReadLine();
-
-                    if (inputMenu == "1")
-                    {
-                        Console.Write("Masukkan index yang ingin diubah: ");
-                        int j = Convert.ToInt32(Console.ReadLine()) - 1;
-
-                        Console.Write("Firstname: ");
-                        firstname = Console.ReadLine();
-                        Console.Write("Lastname: ");
-                        lastname = Console.ReadLine();
+                        Console.WriteLine("User gagal ditambah, pastikan panjang password minimal 8, terdapat minimal 1 huruf kapital dan 1 angka");
                         Console.Write("Password: ");
                         password = Console.ReadLine();
 
-                        username = firstname.Substring(0, 2) + lastname.Substring(0, 2);
+                        cekPassword(password);
+                    }
 
-                        var cek2 = userList.Where(u => u.Username == username);
+                    var user = new User(firstname, lastname, password);
 
-                        if (cek2.Count() != 0)
+                    if (userList.Any(u => u.Username.Contains(user.Username)))
+                    {
+                        user.Username = $"{user.Firstname[..2]}" +
+                                        $"{user.Lastname[..2]}" +
+                                        $"{userList.Count(u => u.Username.ToLower().Contains(user.Username.ToLower()))}";
+                    }
+
+                    userList.Add(user);
+
+                    Console.WriteLine("User berhasil ditambah. Tekan sembarang tombol untuk kembali ke halaman utama");
+                    Console.ReadKey();
+                                                           
+                    break;
+                case "2":
+                    bool endCaseTwo = false;
+
+                    while (!endCaseTwo)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("List of User(s)");
+
+                        int index = 1;
+                        foreach (User u in userList)
                         {
-                            Console.WriteLine("Firstname dan Lastname sudah tersedia");
+                            Console.WriteLine("--------------------------------");
+                            Console.WriteLine("ID: " + index);
+                            Console.WriteLine(u.getUserData());
+                            Console.WriteLine("--------------------------------");
+                            index++;
+                        }
+
+                        Console.WriteLine("Menu.");
+                        Console.WriteLine("1. Edit user");
+                        Console.WriteLine("2. Delete user");
+                        Console.WriteLine("3. Return to main menu");
+                        Console.Write("Your option? ");
+                        inputMenu = Console.ReadLine();
+
+                        if (inputMenu == "1")
+                        {
+                            Console.Write("Masukkan index yang ingin diubah: ");
+                            int j = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                            Console.Write("Firstname: ");
+                            firstname = Console.ReadLine();
+                            Console.Write("Lastname: ");
+                            lastname = Console.ReadLine();
+                            Console.Write("Password: ");
+                            password = Console.ReadLine();
+
+                            while (!cekPassword(password))
+                            {
+                                Console.WriteLine("User gagal ditambah, pastikan panjang password minimal 8, terdapat minimal 1 huruf kapital dan 1 angka");
+                                Console.Write("Password: ");
+                                password = Console.ReadLine();
+
+                                cekPassword(password);
+                            }
+
+                            username = firstname.Substring(0, 2) + lastname.Substring(0, 2);
+
+                            if (userList.Any(u => u.Username.Contains(username)))
+                            {
+                                username = $"{firstname}" +
+                                           $"{lastname}" +
+                                           $"{userList.Count(u => u.Username.ToLower().Contains(username.ToLower()))}";
+                            }
+
+                            userList[j].Username = username;
+                            userList[j].Firstname = firstname;
+                            userList[j].Lastname = lastname;
+                            userList[j].Password = password;
+
+                            Console.WriteLine("User berhasil ditambah. Tekan sembarang tombol...");
+                            Console.ReadKey();
+                        }
+                        else if (inputMenu == "2")
+                        {
+                            Console.Write("Masukkan index yang ingin dihapus.");
+                            int i = Convert.ToInt32(Console.ReadLine()) - 1;
+                            userList.RemoveAt(i);
+                            Console.WriteLine("Data terkait berhasil dihapus. Tekan sembarang tombol...");
+                        }
+                        else if (inputMenu == "3")
+                        {
+                            endCaseTwo = true;
                         }
                         else
                         {
-                            if (password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsNumber))
-                            {
-                                userList[j].Username = username;
-                                userList[j].Firstname = firstname;
-                                userList[j].Lastname = lastname;
-                                userList[j].Password = password;
-
-                                Console.WriteLine("User berhasil ditambah. Tekan sembarang tombol untuk kembali ke halaman utama");
-                                Console.ReadKey();
-                            }
-                            else
-                            {
-                                Console.WriteLine("User gagal ditambah, pastikan panjang password minimal 8, terdapat minimal 1 huruf kapital dan 1 angka");
-                                Console.ReadKey();
-                            }
-                        }
-                    }
-                    else if (inputMenu == "2")
-                    {
-                        Console.Write("Masukkan index yang ingin dihapus.");
-                        int i = Convert.ToInt32(Console.ReadLine()) - 1;
-                        userList.RemoveAt(i);
-                        Console.WriteLine("Data terkait berhasil dihapus. Tekan sembarang tombol untuk kembali ke halaman utama");
-                    }
-                    else if (inputMenu == "3")
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Pilihan tidak tersedia");
-                    }
-
-                    Console.ReadKey();
+                            Console.WriteLine("Pilihan tidak tersedia");
+                            Console.ReadKey();
+                        }                        
+                    }                    
                     break;
                 case "3":
                     Console.Clear();
@@ -157,12 +151,18 @@ class Program
                     Console.WriteLine("WARNING: This is case sensitive");
                     Console.Write("Enter firstname: ");
                     search = Console.ReadLine();
-                    var hasil = userList.Where(u => u.Firstname == search);
+
+                    var hasil = userList.Where(u => u.Firstname.ToLower().Contains(search.ToLower()) || u.Lastname.ToLower().Contains(search.ToLower()));
+
+                    if (hasil.Count() < 1)
+                    {
+                        Console.WriteLine("Hasil tidak ditemukan.");
+                    }
                     foreach (User u in hasil)
                     {
-                        Console.WriteLine("Username: " + u.Username);
-                        Console.WriteLine("Name: " + u.Firstname + " " + u.Lastname);
-                        Console.WriteLine("Password: " + u.Password);
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine(u.getUserData());
+                        Console.WriteLine("--------------------------------");
                     }
                     Console.WriteLine("Tekan sembarang kunci untuk kembali ke halaman utama");
                     Console.ReadKey();
@@ -175,10 +175,16 @@ class Program
                     Console.Write("Password: ");
                     password = Console.ReadLine();
 
-                    var result = userList.SingleOrDefault(u => u.Username == username);
-                    if (password == result.Password)
+                    var result = userList.SingleOrDefault(u => u.Username == username && u.Password == password);
+                    
+                    if (result != null)
                     {
                         Console.WriteLine("Login berhasil. Tekan sembarang kunci untuk kembali ke halaman utama");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login gagal. Tekan sembarang kunci untuk kembali ke halaman utama");
                         Console.ReadKey();
                     }
                                         
@@ -191,5 +197,11 @@ class Program
             }
         }       
         
+    }
+
+    static bool cekPassword(string password)
+    {
+        var cek = password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsNumber);
+        return cek;
     }
 }
--- Auth/User.cs
+++ Auth/User.cs
@@ -6,4 +6,18 @@ class User
     public string Lastname { set; get; }
     public string Password { set; get; }
 
+    public User (string firstname, string lastname, string password)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Password = password;
+        Username = firstname.Substring(0, 2) + lastname.Substring(0, 2);
+    }
+
+    public string getUserData()
+    {
+        return $"Fullname : {Firstname} {Lastname} \n" +
+               $"Username : {Username}\n" +
+               $"Password : {Password}\n";
+    }
 }
