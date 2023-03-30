48f55ac5eeb144e62370f2eb6fedd31d440a2d80
umi98 <umifarida98@gmail.com>
2023-03-30 11:52:01 +0700

    Initial commit

--- Auth/User.cs
+++ Auth/User.cs
@@ -1,11 +1,9 @@
-﻿namespace Auth
+﻿namespace Auth;
+class User
 {
-    class User
-    {
-        public string Username { set; get; }
-        public string Firstname { set; get; }
-        public string Lastname { set; get; }
-        public string Password { set; get; }
+    public string Username { set; get; }
+    public string Firstname { set; get; }
+    public string Lastname { set; get; }
+    public string Password { set; get; }
 
-    }
 }
